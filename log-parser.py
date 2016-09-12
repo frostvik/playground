cbe6a3b97ac0886abc1c2d2cf606da20decd8c1f
dragos.dumitru <dragos.dumitru@ro.ibm.com>
2016-09-12 13:29:32 +0300

    added checks for files
    This versions checks if any filenames are to be updated

--- log-parser.py
+++ log-parser.py
@@ -2,29 +2,61 @@
 #  and appends VM hostname and date to filename
 import datetime
 import os
+
 # olddate = datetime.date.today()
 # if needed, range for digits regex: \d{4,7}
 path = 'put path here'
 basefn = 'put base name here'
 
-def createdate(line):
-    datefromfile = line.split()[0].split('[')[1].__str__()
+# Function to change date format
+def createdate(l):
+    datefromfile = l.split()[0].split('[')[1].__str__()
     # map the var to a date type and tell it the format
-    thedate = datetime.datetime.strptime(datefromfile, '%Y-%m-%d')
+    thedate = datetime.datetime.strptime(datefromfile, '%Y-%m-%d').date()
     # convert it to a string in a different format
     newdate = datetime.date.strftime(thedate, '%Y%m%d')
 
     return newdate
 
-for file in os.listdir(path):
-    if file.startswith(basefn):
+
+# Function to update base naming
+def basename(l):
+    basefn = ''
+    if 'Starting pre-provisioning workflow' in l:
+        basefn = 'log_0_preprov_workflow'
+    elif 'Starting post-provisioning workflow' in l:
+        basefn = 'log_1_postprov_workflow'
+    elif 'Starting decommissioning workflow' in l:
+        basefn = 'log_2_decomm_workflow'
+    elif 'Fetching a work item' in l:
+        basefn = 'log_gugent'
+    return basefn
+
+
+found = False
+for logfile in os.listdir(path):
+    if logfile.startswith(basen):
+        found = True
+if not found:
+    print 'No log file found!'
+
+count = 0
+for logfile in os.listdir(path):
+    if logfile.startswith(basen):
         os.chdir(path)
-        for line in open(file):
-            if 'put search string here' in line:
-            # if 'Hostname' in line:
-                host = line.split()[7]
-                # host = line.split()[5]
-                mydate = createdate(line)
-                os.rename(file, basefn+'_'+mydate+'_'+host+'.log')
+        logname = ''
+        for logline in open(logfile):
+            if basename(logline):
+                logname = basename(logline)
                 break
-        print file
+        for logline in open(logfile):
+            # if 'Hostname' in logline:
+            if 'Found property Hostname' in logline:
+                # host = logline.split()[5]
+                host = logline.split()[7]
+                mydate = createdate(logline)
+                os.rename(logfile, logname + '_' + mydate + '_' + host + '.log')
+                count += 1
+                break
+count = count.__str__()
+print count + ' files modified.'
